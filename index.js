9db3bfc2c56c8986416b298868d3e3d54364a775
Lucas Duncan <lucasdonuts@gmail.com>
2022-07-14 11:10:21 -0500

    commit tree d667ea1819dd7c5a9b34dc3a4b1786c5d6d004ff

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your code here
+class Breakfast {
+  constructor(food, drink) {
+    this.food = food;
+    this.drink = drink;
+  }
+}
+
+class Lunch {
+  constructor(salad, soup, drink) {
+    this.soup = soup;
+    this.salad = salad;
+    this.drink = drink;
+  }
+}
+
+class Dinner {
+  #dessert;
+  constructor(salad, soup, entree, dessert) {
+    this.soup = soup;
+    this.salad = salad;
+    this.entree = entree;
+    this.#dessert = dessert;
+  }
+}
